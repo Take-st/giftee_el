1c9847be0f1de212a4992031785fccc5bd36f029
Take-st <s.takewuw@gmail.com>
2019-04-25 15:07:19 +0900

    Slight change

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,4 +18,3 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery-ui
-//= require tag-it
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -23,7 +23,7 @@ class SessionsController < ApplicationController
   def destroy
     reset_session
     flash[:success] = 'ログアウトしました'
-    redirect_to root_path
+    redirect_to login_path
   end
 
   private
